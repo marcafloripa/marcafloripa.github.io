0a92b9d65e7371fe1bf7849a693eaa8ea141f81f
Felipe Ruhland <felipe.ruhland@gmail.com>
2015-07-16 16:14:49 -0300

    update

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
-    $('#show-form').on('click', function() {
-        $('#form').fadeIn();
+    $('.show-names').on('click', function() {
+        $('.assinaturas').fadeIn();
         $('.logos-img').fadeOut();
     });
-    $('#close-form').on('click', function() {
-    	$('#form').fadeOut();
+    $('#voltar').on('click', function() {
+    	$('.assinaturas').fadeOut();
         $('.logos-img').fadeIn();
+    });
+
+    window.whoSigned = function(d){
+        var $names = $("<ol />");
+        $.each(d.feed.entry,function(i,line){
+            $("<li />").text(line.title["$t"]).appendTo($names);
+        });
+        $names.appendTo(".apoios .assinaturas");
+    };
+
+    var url = "https://spreadsheets.google.com/feeds/list/1NtVELi63fKBFK7TcEbxUdlqMlTG1D_x36PznLTXaaEE/001/public/basic?alt=json-in-script&callback=whoSigned";
+    $.ajax({
+        url: url,
+        type:"GET",
+        dataType:"script",
+        jsonp: "myFunc"
     });
 });
